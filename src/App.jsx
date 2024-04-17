50c7e231e62a7c3760913b27b29d7e3b4ad381fe
Dor Katzir <katzirdor@gmail.com>
2024-04-17 14:09:03 +0300

    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,13 @@ const App = () =>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
-          <div className={`styles.boxWidth`}>
+          <div className={styles.boxWidth}>
             <Hero />
           </div>
       </div>
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-          <div className={`styles.boxWidth`}>
+          <div className={styles.boxWidth}>
             <Stats />
             <Business />
             <Billing />
