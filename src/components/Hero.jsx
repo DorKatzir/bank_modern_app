fbbc480bb56b32c2528f6b9bbe1a0088a3aa80f5
Dor Katzir <katzirdor@gmail.com>
2024-04-17 14:39:25 +0300

    Hero Section: left side finished

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,9 @@ import GetStarted from './GetStarted.jsx'
 
 const Hero = () =>  (
     <section className={`flex md:flex-row flex-col ${styles.paddingY}`}>
-      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
 
+      {/* LEFT SIDE */}
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
         <div className="flex flex-row items-canter py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
           <img src={discount} alt="discount" className='w-[32px] h-[32px]'/>
           <p className={`${styles.paragraph} ml-2`}>
@@ -17,15 +18,27 @@ const Hero = () =>  (
         <div className="flex flex-row justify-between items-center w-full">
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
             The Next <br className="sm:block hidden" />
-            <span className="text-gradient">Generation</span> Payment Method.
+            <span className="text-gradient">Generation</span>
           </h1>
           <div className="ss:flex hidden md:mr-4 mr-0">
             <GetStarted />
           </div>
         </div>
 
+        <h2 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full">
+          Payment Method.
+        </h2>
 
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Our team of experts uses a methodology to identify the credit cards
+          most likely to fit your needs. We examine annual percentage rates,
+          annual fees.
+        </p>
       </div>
+
+      {/* RIGHT SIDE */}
+      
+
     </section>
   )
 
