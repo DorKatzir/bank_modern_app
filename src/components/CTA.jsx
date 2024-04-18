6657adc2b5d134253bfaf8c558ff46952aa90825
Dor Katzir <katzirdor@gmail.com>
2024-04-18 18:13:26 +0300

    CTA section completed

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -10,6 +10,9 @@ const CTA = () => (
           anywhere on the planet.
         </p>
       </div>
+      <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
+        <Button />
+      </div>
     </section>
   )
 
